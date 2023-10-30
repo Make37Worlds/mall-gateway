f0780bf7ce42f41f1b13011c41f3ebd08578e6b1
Make37Worlds <515118458@qq.com>
2023-10-31 02:20:00 +0800

    cors

--- src/main/java/com/example/mallgateway/CorsConfig.java
+++ src/main/java/com/example/mallgateway/CorsConfig.java
@@ -1,24 +1,23 @@
 package com.example.mallgateway;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
-public class CorgsConfig {
+public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedMethod("*");
-        config.addAllowedOrigin("http://13.212.107.51:30385");
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
+        config.addAllowedMethod("*");  // 允许所有方法
+        config.addAllowedOrigin("http://172.31.11:30385");  // 允许的源
+        config.addAllowedHeader("*");  // 允许所有头信息
+        config.setAllowCredentials(false);  // 不允许携带凭证
+        config.setMaxAge(3600L);  // 预检请求的结果缓存时间（秒）
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
         source.registerCorsConfiguration("/**", config);
