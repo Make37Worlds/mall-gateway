55f8d69e0ac62f4f258d1f21db6664bf03e64325
Make37Worlds <515118458@qq.com>
2023-11-12 00:53:00 +0800

    gateway authentication

--- src/main/java/SecurityConfig.java
+++ src/main/java/SecurityConfig.java
@@ -1,5 +1,3 @@
-package com.example.mallgateway;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
